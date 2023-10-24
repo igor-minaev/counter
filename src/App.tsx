e7e83bb3bc6d190fc593481dcd9dfb46da601bfa
Igor Minayeu <igor.minayeu@gmail.com>
2023-10-24 21:49:18 +0300

    start counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let startValue = 0
+    let maxValue = 5
+    const [count, setCount] = useState(0)
+    return (
+        <div className="App">
+            <div>{count}</div>
+            <button disabled={count === maxValue} onClick={() => setCount(count + 1)}>inc</button>
+            <button disabled={count === startValue} onClick={() => setCount(0)}>reset</button>
+        </div>
+    );
 }
 
 export default App;
