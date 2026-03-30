fd6da628bbb4d0898e968e9a37aa1bcd5102c116
Igor Minayeu <igor.minayeu@gmail.com>
2026-03-30 20:23:25 +0300

    add Counter component

---
+++ src/Counter.tsx
@@ -0,0 +1,26 @@
+import {CounterScreen} from "./CounterScreen.tsx";
+import {Button} from "./components/Button.tsx";
+import {useState} from "react";
+
+export const Counter = () => {
+    const startValue = 0
+    const maxValue = 5
+    const [counterValue, setCounterValue] = useState(startValue)
+
+    const increaseCounterValue = () => counterValue < maxValue && setCounterValue(counterValue + 1)
+    const resetCounterValue = () => setCounterValue(startValue)
+
+    const isIncreaseDisabledBtn = counterValue === maxValue
+    const isResetDisabledBtn = counterValue === startValue
+
+    return (
+        <div className='counter'>
+            <CounterScreen counterValue={counterValue} maxValue={maxValue}/>
+            <div className='buttonsWrapper'>
+                <Button className='button' disabled={isIncreaseDisabledBtn} onClick={increaseCounterValue}>inc</Button>
+                <Button className='button' disabled={isResetDisabledBtn} onClick={resetCounterValue}>reset</Button>
+            </div>
+        </div>
+    );
+};
+
