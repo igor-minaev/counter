cf6b1ffc1775369aa1b482585534a5c6eca0c58c
Igor Minayeu <igor.minayeu@gmail.com>
2026-03-30 18:34:48 +0300

    start counter project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {useState} from "react";
+
+function App() {
+    let startValue = 0
+    let maxValue = 5
+    const [counterValue, setCounterValue] = useState(startValue)
+    return (
+        <div className="app">
+
+        </div>
+    )
+}
+
+export default App
