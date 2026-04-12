174596e5cb5e5aaac541d9b457a315f6432ad954
Igor Minayeu <igor.minayeu@gmail.com>
2026-04-12 12:32:38 +0300

    move functions for localStorage

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,14 +1,8 @@
 import {useState} from "react";
 import {Settings} from "./Settings.tsx";
 import {CounterView} from "./CounterView.tsx";
+import {getValuesFromLocalStarage, setValuesToLocalStorage} from "./utilsLocalStarage.ts";
 
-const getValues = (key: 'startValue' | 'maxValue', defaultValue: number) => {
-    const startValueAsString = localStorage.getItem(key)
-    if (startValueAsString) {
-        return JSON.parse(startValueAsString)
-    }
-    return defaultValue
-}
 
 const DEFAULT_START_VALUE = 0
 const DEFAULT_MAX_VALUE = 5
@@ -16,8 +10,8 @@ const DEFAULT_MAX_VALUE = 5
 
 export const Counter = () => {
 
-    const [startValue, setStartValue] = useState(getValues("startValue", DEFAULT_START_VALUE))
-    const [maxValue, setMaxValue] = useState(getValues('maxValue', DEFAULT_MAX_VALUE))
+    const [startValue, setStartValue] = useState(getValuesFromLocalStarage("startValue", DEFAULT_START_VALUE))
+    const [maxValue, setMaxValue] = useState(getValuesFromLocalStarage('maxValue', DEFAULT_MAX_VALUE))
     const [counterValue, setCounterValue] = useState(startValue)
     const [settingsMode, setSettingsMode] = useState(false)
 
@@ -29,8 +23,8 @@ export const Counter = () => {
     const openSettings = () => setSettingsMode(true)
 
     const setSettings = (startValue: number, maxValue: number) => {
-        localStorage.setItem('startValue', JSON.stringify(startValue))
-        localStorage.setItem('maxValue', JSON.stringify(maxValue))
+        setValuesToLocalStorage('startValue', startValue)
+        setValuesToLocalStorage('maxValue', maxValue)
         setStartValue(startValue)
         setMaxValue(maxValue)
         setCounterValue(startValue)
---
+++ src/utilsLocalStarage.ts
@@ -0,0 +1,12 @@
+export const getValuesFromLocalStarage = (key: 'startValue' | 'maxValue', defaultValue: number) => {
+    const startValueAsString = localStorage.getItem(key)
+    if (startValueAsString) {
+        return JSON.parse(startValueAsString)
+    }
+    return defaultValue
+}
+
+export const setValuesToLocalStorage = (key: 'startValue' | 'maxValue', value: number) => {
+
+    localStorage.setItem(key, JSON.stringify(value))
+}
